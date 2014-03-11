b4833b0db5ebbdcf166d01c27e0944f3c6725790
minhhai3b <minhhai3b@gmail.com>
2014-03-11 18:42:33 +0900

    iOS

--- iOS/FlappyBird/FlappyBird/Classes/MenuScene.m
+++ iOS/FlappyBird/FlappyBird/Classes/MenuScene.m
@@ -40,18 +40,28 @@
     [self addChild:background];
     NSArray *footerinfo = [atlasInfo objectForKey:@"land"];
     CCSprite *footer = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[footerinfo objectAtIndex:3] intValue], [[footerinfo objectAtIndex:4] intValue], [[footerinfo objectAtIndex:1] intValue], [[footerinfo objectAtIndex:2] intValue])];
-//    footer.scaleX = scaleX;
-//    footer.scaleY = scaleY;
-    NSLog(@"scene: %f; footer: %f", scaleX, scaleY);
+    CCSprite *footer_1 = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[footerinfo objectAtIndex:3] intValue], [[footerinfo objectAtIndex:4] intValue], [[footerinfo objectAtIndex:1] intValue], [[footerinfo objectAtIndex:2] intValue])];
     footer.position = ccp(footer.contentSize.width / 2, footer.contentSize.height / 2);
     [self addChild:footer];
+    footer_1.position = ccp(self.contentSize.width + footer.contentSize.width / 2, footer.position.y);
+    [self addChild:footer_1];
     NSArray *copyinfo = [atlasInfo objectForKey:@"brand_copyright"];
     CCSprite *copy = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[copyinfo objectAtIndex:3] intValue], [[copyinfo objectAtIndex:4] intValue], [[copyinfo objectAtIndex:1] intValue], [[copyinfo objectAtIndex:2] intValue])];
     copy.position = ccp(self.contentSize.width / 2, footer.contentSize.height / 2 );
     [self addChild:copy];
     // animate footer
-    id footerAction = [CCActionRepeatForever actionWithAction:[CCActionSequence actionWithArray:@[[CCActionMoveTo actionWithDuration:0.3 position:CGPointMake(self.contentSize.width - footer.contentSize.width / 2, footer.position.y)], [CCActionMoveTo actionWithDuration:0.0 position:CGPointMake(footer.contentSize.width / 2, footer.position.y)]]]];
-    [footer runAction:footerAction];
+    float footerV = 200.0;
+    id footerAction = [CCActionRepeatForever actionWithAction: [CCActionSequence actionWithArray:@[[CCActionMoveTo actionWithDuration:(footer.contentSize.width * 2)/footerV position:CGPointMake(-footer.contentSize.width / 2, footer.position.y)], [CCActionMoveTo actionWithDuration:0.0 position:CGPointMake(footer_1.position.x + footer.contentSize.width / 2, footer.position.y)]]]];
+    CCActionCallBlock *actionBlock = [CCActionCallBlock actionWithBlock:^{
+        [footer_1 runAction:[CCActionSequence actionWithArray:@[[CCActionMoveTo actionWithDuration:0.5 position:CGPointMake(self.contentSize.width - footer.contentSize.width / 2, footer.position.y)], [CCActionMoveTo actionWithDuration:0.0 position:CGPointMake(self.contentSize.width + footer.contentSize.width / 2, footer.position.y)], actionBlock_1]]];
+    }];
+    CCActionCallBlock *actionBlock_1 = [CCActionCallBlock actionWithBlock:^{
+        [footer runAction:footerAction];
+    }];
+    [footer runAction:[CCActionSequence actionWithArray:@[[CCActionMoveTo actionWithDuration:0.5 position:CGPointMake(self.contentSize.width - footer.contentSize.width / 2, footer.position.y)], [CCActionMoveTo actionWithDuration:0.0 position:CGPointMake(self.contentSize.width + footer.contentSize.width / 2, footer.position.y)], actionBlock]]];
+//    [footer runAction:[CCActionSequence actionWithArray:@[[CCActionMoveTo actionWithDuration:(footer.contentSize.width - self.contentSize.width)/footerV position:CGPointMake(-footer.contentSize.width / 2, footer.position.y)],[CCActionMoveTo actionWithDuration:0.0 position:CGPointMake(footer.contentSize.width * 1.5, footer.position.y)], actionBlock]]];
+//    [footer runAction:footerAction];
+//    [footer_1 runAction:footerAction];
 	
     // done
 	return self;
