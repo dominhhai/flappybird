ed9bfa69da19cae4deb23f03c7c4028c47634871
minhhai3b <minhhai3b@gmail.com>
2014-03-14 21:23:08 +0900

    iOS: completed footer

---
+++ Android/FlappyBird/gen/vn/com/minhhai3b/flappybird/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package vn.com.minhhai3b.flappybird;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- iOS/FlappyBird/FlappyBird/Classes/MenuScene.m
+++ iOS/FlappyBird/FlappyBird/Classes/MenuScene.m
@@ -43,8 +43,8 @@ CCSprite *footer_1;
     footer = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[footerinfo objectAtIndex:3] intValue], [[footerinfo objectAtIndex:4] intValue], [[footerinfo objectAtIndex:1] intValue], [[footerinfo objectAtIndex:2] intValue])];
     footer_1 = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[footerinfo objectAtIndex:3] intValue], [[footerinfo objectAtIndex:4] intValue], [[footerinfo objectAtIndex:1] intValue], [[footerinfo objectAtIndex:2] intValue])];
     footer.position = ccp(footer.contentSize.width / 2, footer.contentSize.height / 2);
-    [self addChild:footer];
     footer_1.position = ccp(footer.contentSize.width * 3 / 2, footer.position.y);
+    [self addChild:footer];
     [self addChild:footer_1];
     NSArray *copyinfo = [atlasInfo objectForKey:@"brand_copyright"];
     CCSprite *copy = [CCSprite spriteWithTexture:atlas rect:CGRectMake([[copyinfo objectAtIndex:3] intValue], [[copyinfo objectAtIndex:4] intValue], [[copyinfo objectAtIndex:1] intValue], [[copyinfo objectAtIndex:2] intValue])];
@@ -57,22 +57,25 @@ CCSprite *footer_1;
 -(void) update:(CCTime)delta {
 	CGPoint bg1Pos = footer.position;
 	CGPoint bg2Pos = footer_1.position;
-	bg1Pos.x -= 1;
+    
+//    NSLog(@"Before: %f; %f; size: %f", bg1Pos.x, bg2Pos.x, footer.contentSize.width /  2);
+    
+    bg1Pos.x -= 1;
 	bg2Pos.x -= 1;
 	
 	// move scrolling background back by one screen width to achieve "endless" scrolling
-	if (bg1Pos.x < (self.contentSize.width - footer.contentSize.width / 2)) {
-		bg1Pos.x = bg2Pos.x + footer.contentSize.width / 2;
+	if (bg1Pos.x < - footer.contentSize.width / 2) {
+		bg1Pos.x = bg2Pos.x + footer.contentSize.width;
+	} else if (bg2Pos.x < - footer_1.contentSize.width / 2) {
+		bg2Pos.x = bg1Pos.x + footer_1.contentSize.width;
 	}
-    else if (bg2Pos.x < (self.contentSize.width - footer.contentSize.width / 2)) {
-		bg2Pos.x = bg1Pos.x + footer.contentSize.width / 2;
-	}
+
+//    NSLog(@"After: %f; %f; size: %f", bg1Pos.x, bg2Pos.x, footer.contentSize.width /  2);	
     
-	
 	// remove any inaccuracies by assigning only int values
 	// (prevents floating point rounding errors accumulating over time)
-	bg1Pos.x = (int)bg1Pos.x;
-    bg2Pos.x = (int)bg2Pos.x;
+//	bg1Pos.x = (int)bg1Pos.x;
+//    bg2Pos.x = (int)bg2Pos.x;
     footer.position = bg1Pos;
     footer_1.position = bg2Pos;
 }
